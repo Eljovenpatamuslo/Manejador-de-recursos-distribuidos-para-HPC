c9f4da7fe839f32963e5f73549de521327ae15d6
francobramucci <bramuccifranco1@gmail.com>
2026-06-22 17:58:52 -0300

    Cambios

--- estructuras/tablajobs.h
+++ estructuras/tablajobs.h
@@ -84,15 +84,19 @@ unsigned long tablajobs_restar_recurso(TablaJobs tabla, unsigned long jobId,
 
 int tablajobs_job_granted(TablaJobs tabla, unsigned long jobId);
 
-void registrar_solicitud_propia(TablaJobs tablaJobs, unsigned long jobId, TipoRecurso rec,
-                                unsigned long cant, const char* ipDestino, unsigned short puertoDestino, void *datosCliente);
+void registrar_solicitud_propia(TablaJobs tablaJobs, unsigned long jobId,
+                                TipoRecurso rec, unsigned long cant,
+                                const char *ipDestino,
+                                unsigned short puertoDestino,
+                                void *datosCliente);
 
-void tablajobs_recurso_granted(TablaJob tabla, unsigned long jobId, char ip[], unsigned short puerto);
+void tablajobs_recurso_granted(TablaJob tabla, unsigned long jobId, char ip[],
+                               unsigned short puerto);
 
 ListaResultados tablajobs_release_job(TablaJobs tabla, unsigned long jobId);
 
 void desconectar_job(TablaJobs tabla, JobActivo *job);
 
-void liberar_memoria_job(JobActivo* job);
+void liberar_memoria_job(JobActivo *job);
 
 #endif /* _TABLAJOBS_H_ */
